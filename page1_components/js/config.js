e805a6f8bc789df498bf9fe705f4fda26d7aad86
kaisoccer19960414-cmd <kaisoccer19960414@gmail.com>
2026-07-03 20:20:23 +0900

    page1

--- page1_components/js/config.js
+++ page1_components/js/config.js
@@ -1,2 +1,2 @@
 export const SUPABASE_URL = 'https://tekrwutayfleorpfbuhc.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGci...'; // 実際のキー
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRla3J3dXRheWZsZW9ycGZidWhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NzA1ODIsImV4cCI6MjA5ODE0NjU4Mn0.eG8ENxN1BxZn_yFdxrsytz2Qa9LCT95WgdRqLkEDs80';

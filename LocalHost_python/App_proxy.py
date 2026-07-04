e81ec3c123ee5826d09ce5ffff02fb8a5553d3b4
kaisoccer19960414-cmd <kaisoccer19960414@gmail.com>
2026-07-05 07:49:56 +0900

    Proxy

--- LocalHost_python/App_proxy.py
+++ LocalHost_python/App_proxy.py
@@ -25,7 +25,7 @@ SUPABASE_HEADERS = {
 
 def check_passcode():
     """リクエストヘッダーのパスコードが正しいか確認する"""
-    given = request.headers.get('X-App-Passcode')
+    given = request.headers.get('X-App-Passcode': '1111')
     return given == APP_PASSCODE
 
 

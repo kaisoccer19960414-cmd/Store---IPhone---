e188c6fb428dbdc1d514a4b75729ec0bcd1b66ad
kaisoccer19960414-cmd <kaisoccer19960414@gmail.com>
2026-07-05 09:25:42 +0900

    Render proxy

--- page1_components/js/config.js
+++ page1_components/js/config.js
@@ -1,5 +1,5 @@
 
-export const USE_LOCAL_API = true; // trueならFlask版、falseならSupabase版
+export const USE_LOCAL_API = true; // trueならFlask版、falseならSupabase版 (超重要！！)
 
 
 // Supabase(クラウド版)
@@ -7,5 +7,9 @@ export const SUPABASE_URL = 'https://tekrwutayfleorpfbuhc.supabase.co';
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRla3J3dXRheWZsZW9ycGZidWhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NzA1ODIsImV4cCI6MjA5ODE0NjU4Mn0.eG8ENxN1BxZn_yFdxrsytz2Qa9LCT95WgdRqLkEDs80';
 
 
-// ローカルFlask版
+/* ローカルFlask版
 export const LOCAL_API_URL = 'http://127.0.0.1:5000';
+*/
+
+// 変更後(Renderで発行された本物のURLに置き換え)
+export const LOCAL_API_URL = 'https://store-iphone-xxxx.onrender.com';
